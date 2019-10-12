b5141bba908bf816f6ecbc1b5d2b2e0b64b41f6a
Theresa O'Hare <theresaohare@Theresas-MacBook-Air.local>
2019-10-12 16:38:32 -0600

    Remove fodder and replace todo layout

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <NameGenerator title="Name Generator"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NameGenerator from './components/NameGenerator.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    NameGenerator
   }
 }
 </script>
