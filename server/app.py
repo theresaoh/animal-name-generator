50587a13472dfe562a68a7f1526c7bc59fbc3d9b
Theresa OHare <theresa.ohare@outlook.com>
2019-10-12 17:15:59 -0600

    Flask now serving vue app

--- server/app.py
+++ server/app.py
@@ -1,9 +1,30 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template, jsonify
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+names = ['Nap Time All-Star', "Phil", "Theresa"]
+
+app = Flask(__name__,
+    static_folder = "./dist/static",
+    template_folder = "./dist"
+    )
+
+@app.route('/')
+def serve_vue_app():
+    """
+    Serve our Vue App
+    """
+    return render_template('index.html')
+
+@app.after_request
+def add_header(req):
+    """
+    Clear Cache for hot-reloading
+    """
+    req.headers["Cache-Control"] = "no-cache"
+    return req
+
+@app.route('/names', methods=['GET'])
+def serve_all_names():
+    return jsonify({"results": names})
 
 if __name__ == "__main__":
     app.run(debug = True)
