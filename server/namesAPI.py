a8498c5719db83dd4d89651e9cc668797ca0dffb
Theresa O'Hare <theresaohare@Theresas-MacBook-Air.local>
2019-10-21 23:02:55 -0600

    separate apis are now created to serve 10 random names by sex to FE

--- server/namesAPI.py
+++ server/namesAPI.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, jsonify, request
+from sqlalchemy import func
 from sqlalchemy_db_instance import db
 from models import Name
 import pandas as pd
@@ -10,6 +11,24 @@ names_api = Blueprint('names_api', __name__)
 @names_api.route('/names', methods=['GET'])
 def serve_all_names():
     name_instances = db.session.query(Name).all()
+    name_results = [{"id": name.id, "name": name.AnimalName, "gender": name.Sex} for name in name_instances]
+    return jsonify({"name": name_results})
+
+@names_api.route('/male-names', methods=['GET'])
+def serve_male_names():
+    name_instances = db.session.query(Name).filter(Name.Sex == "M").order_by(func.random()).limit(10)
+    name_results = [{"id": name.id, "name": name.AnimalName, "gender": name.Sex} for name in name_instances]
+    return jsonify({"name": name_results})
+
+@names_api.route('/female-names', methods=['GET'])
+def serve_female_names():
+    name_instances = db.session.query(Name).filter(Name.Sex == "F").order_by(func.random()).limit(10)
+    name_results = [{"id": name.id, "name": name.AnimalName, "gender": name.Sex} for name in name_instances]
+    return jsonify({"name": name_results})
+
+@names_api.route('/gender-neutral-names', methods=['GET'])
+def serve_gender_neutral_names():
+    name_instances = db.session.query(Name).filter(Name.Sex == "GN").order_by(func.random()).limit(10)
     name_results = [{"id": name.id, "name": name.AnimalName, "gender": name.Sex} for name in name_instances]
     return jsonify({"name": name_results})
 
