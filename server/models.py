d2886480cec5c3afd5a97c3159aff1a0046a6a57
Theresa O'Hare <theresaohare@Theresas-MacBook-Air.local>
2019-10-25 16:58:57 -0600

    moved setup_database to models.py

--- server/models.py
+++ server/models.py
@@ -16,3 +16,20 @@ class Favorites(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.String(35), db.ForeignKey('user_table.id'))
     name_id = db.Column(db.String(35), db.ForeignKey('name_table.id'))
+
+def setup_database(app):
+    with app.app_context():
+        engine = db.get_engine()
+        Base.metadata.create_all(engine)
+
+        csv_file_path = 'Names.csv'
+
+        # Read CSV with Pandas
+        with open(csv_file_path, 'r') as file:
+            df = pd.read_csv(file)
+
+        # Insert to DB
+        df.to_sql('name_table',
+                con=engine,
+                index_label='id',
+                if_exists='replace')
