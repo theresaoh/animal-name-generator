13b71cb754d034921eb86ceea73ace48c5ec67ee
Theresa OHare <theresa.ohare@outlook.com>
2019-10-28 16:25:46 -0600

    commented out unnecessary seed data

--- server/models.py
+++ server/models.py
@@ -23,15 +23,15 @@ def setup_database(app):
     with app.app_context():
         db.create_all()
 
-        engine = db.get_engine()
-        csv_file_path = 'Names.csv'
+        # engine = db.get_engine()
+        # csv_file_path = 'Names.csv'
 
-        # Read CSV with Pandas
-        with open(csv_file_path, 'r') as file:
-            df = pd.read_csv(file)
+        # # Read CSV with Pandas
+        # with open(csv_file_path, 'r') as file:
+        #     df = pd.read_csv(file)
 
-        # Insert to DB
-        df.to_sql('name_table',
-                con=engine,
-                index_label='id',
-                if_exists='replace')
+        # # Insert to DB
+        # df.to_sql('name_table',
+        #         con=engine,
+        #         index_label='id',
+        #         if_exists='replace')
