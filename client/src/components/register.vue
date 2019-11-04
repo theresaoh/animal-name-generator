2cef4903ad3369c2c9f4f3f8624f4d236fede296
Theresa O'Hare <theresaohare@Theresas-MacBook-Air.local>
2019-11-03 21:11:03 -0700

    improved registration requirements and error messages

--- client/src/components/register.vue
+++ client/src/components/register.vue
@@ -5,7 +5,9 @@
       <p>Username:</p><input v-model="username" type="text" />
       <p>Password:</p><input type="password" v-model="password"/>
       <p>Confirm Password:</p><input type="password" v-model="confirmPassword"/><br><br>
-      <div class="error-message">{{ errorMessage }}</div>
+      <div class="error-message">{{ duplicateUserErrorMessage }}</div>
+      <div class="error-message">{{ passwordMatchErrorMessage }}</div>
+      <div class="error-message">{{ passwordRequirementsErrorMessage }}</div>
       <button @click="testDuplicateUser()">Register</button>
       <p>Already a registered user?</p>
       <router-link :to="'/login'"><button>Log In</button></router-link>
@@ -26,18 +28,39 @@ export default {
       username: '',
       password: '',
       confirmPassword: '',
-      errorMessage: ''
+      duplicateUserErrorMessage: '',
+      passwordRequirementsErrorMessage: '',
+      passwordMatchErrorMessage: ''
     }
   },
   methods: {
+    passwordMeetsRequirements(){
+      this.duplicateUserErrorMessage = '';
+      this.passwordRequirementsErrorMessage = '';
+      this.passwordMatchErrorMessage = '';
+      const capitalLetters = /[A-Z]/
+      const specialChracters = /\W/
+      console.log(capitalLetters.test(this.password));
+      console.log(specialChracters.test(this.password));
+      console.log(this.password.length >=7);
+      if (!capitalLetters.test(this.password) || !specialChracters.test(this.password) || !this.password.length >= 7){
+        this.passwordRequirementsErrorMessage = "Passwords must be at least 5 characters long and include at least one capital letter and one special character";
+        return;
+      }
+      if (this.password != this.confirmPassword){
+        this.passwordMatchErrorMessage = "Those passwords don't match. Please re-enter.";
+        return;
+      }
+      this.addNewUser();
+    },
     testDuplicateUser(){
       axios.post('/duplicate-user-test', { username: this.username })
       .then(resp => {
         console.log(resp.data.does_the_user_exist.length)
         if (resp.data.does_the_user_exist.length == 0){
-          this.addNewUser();
+          this.passwordMeetsRequirements();
         } else {
-          this.errorMessage = "Sorry, that username already exists."
+          this.duplicateUserErrorMessage = "Sorry, that username already exists."
           this.username = '';
           this.password = '';
           this.confirmPassword = '';
@@ -57,12 +80,11 @@ export default {
     login(user, pass) {
       axios.post('/login', {username: user, password: pass})
       .then((resp) => {
+        console.log(resp);
         if (resp.data.success == false){
           this.$router.go();
         } else {
-          this.$router.push('/')
           this.$router.push('/');
-          this.$router.go();
         }
       });
     }
