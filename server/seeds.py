012e3825b796471236701cfbf359cef82c0b8dcd
Theresa O'Hare <theresaohare@Theresas-MacBook-Air.local>
2019-11-18 00:18:36 -0700

    adjusted seeds.py until issue can be resolved with file

--- server/seeds.py
+++ server/seeds.py
@@ -1,16 +1,21 @@
 import pandas as pd
+from sqlalchemy_db_instance import db
+from app_config import app
 
 def seed():
-    engine = db.get_engine()
-    csv_file_path = 'Names.csv'
+    with app.app_context():
+        engine = db.get_engine()
+        csv_file_path = 'Names.csv'
 
-    # Read CSV (Names.csv) with Pandas
-    with open(csv_file_path, 'r') as file:
-        df = pd.read_csv(file)
+        # Read CSV (Names.csv) with Pandas
+        with open(csv_file_path, 'r') as file:
+                df = pd.read_csv(file)
 
-    # Insert to DB
-    df.to_sql('name_table',
-            con=engine,
-            index_label='id',
-            if_exists='append',
-            chunksize=10)
+        # Insert to DB
+        df.to_sql('name_table',
+                con=engine,
+                index_label='id',
+                if_exists='append',
+                chunksize=100
+                )
+        
