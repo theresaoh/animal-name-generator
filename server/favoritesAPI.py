122545d5dc6590d727d248b6932daaa5e2a08622
pkurjanowicz5 <pkurjanowicz5@gmail.com>
2019-11-28 09:31:40 -0700

    fixed duplicate favorite name bug

--- server/favoritesAPI.py
+++ server/favoritesAPI.py
@@ -6,15 +6,21 @@ favorites_api = Blueprint('favorites_api', __name__)
 
 @favorites_api.route('/favorite-name', methods=["POST", "GET"])
 def add_favorite():
-    # add a favorited name to the Favorite table in the DB
-    new_fav = Favorite()
-    new_fav.user_username = session['user']
-    new_fav.name_id = request.json["name_id"]
-    new_fav.favorited_name = request.json["favorited_name"]
-    new_fav.name_gender = request.json["name_gender"]
-    db.session.add(new_fav)
-    db.session.commit()
-    return jsonify(success = True)
+    # first make sure name doesn't exist in DB
+    name = request.json["favorited_name"]
+    try:
+        find_name = Favorite.query.filter_by(favorited_name=name).first().favorited_name
+        return jsonify(duplicate_name=True)
+    except AttributeError:
+        # add a favorited name to the Favorite table in the DB
+        new_fav = Favorite()
+        new_fav.user_username = session['user']
+        new_fav.name_id = request.json["name_id"]
+        new_fav.favorited_name = request.json["favorited_name"]
+        new_fav.name_gender = request.json["name_gender"]
+        db.session.add(new_fav)
+        db.session.commit()
+        return jsonify(success = True)
 
 @favorites_api.route('/favorited-names', methods=['GET'])
 def serve_favorited_names():
@@ -22,6 +28,7 @@ def serve_favorited_names():
     username = session['user']
     favorite_instances = db.session.query(Favorite).filter(Favorite.user_username == username).all()
     favorite_results = [{"id": favorite.id, "user_username": favorite.user_username, "name_id": favorite.name_id, "favorited_name": favorite.favorited_name, "name_gender": favorite.name_gender} for favorite in favorite_instances]
+    print(favorite_results)
     return jsonify({"favorites": favorite_results})
 
 @favorites_api.route('/remove-favorite', methods=['POST'])
